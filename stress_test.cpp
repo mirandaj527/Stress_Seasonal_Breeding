dae112abc476011628476acad279540faf02f035
Miranda Johnstone <mk.chicot@gmail.com>
2025-02-23 18:41:27 +0000

    Update stress_test.cpp
    - reduced maxfitdiff convergence criterion to be 0.001

--- stress_test.cpp
+++ stress_test.cpp
@@ -620,7 +620,7 @@ int main()
           ReplaceFit();
 	  ReplaceHormone();
 
-          if (maxfitdiff < 0.01) 
+          if (maxfitdiff < 0.001) 
             {
                cout << "Converged at iteration: " << i << ", maxfitdiff: " << maxfitdiff << endl;
                break; // strategy has converged on optimal solution, so exit loop
